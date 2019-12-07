cb7aec1b17d0393d171b26e260d198ca2729cc7d
faktaarief <faktaarief16@gmail.com>
2019-12-07 17:54:16 +0700

    New

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+class AdminController extends CI_Controller 
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function list_lagu() 
+    {
+        
+    }
+
+}
